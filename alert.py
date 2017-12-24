7e2403c7506733af9fc8cf1a12a993e7c48e34e5
arhi3a <22037945+arhi3a@users.noreply.github.com>
2017-12-24 19:32:38 +0100

    Add files via upload

--- alert.py
+++ alert.py
@@ -13,6 +13,11 @@ dts = {"/Lotus/Types/Enemies/Acolytes/HeavyAcolyteAgent": 0,
        "/Lotus/Types/Enemies/Acolytes/ControlAcolyteAgent": 0,
        }
 
+nods = {'/Lotus/Types/Enemies/Acolytes/HeavyAcolyteAgent': '',
+        '/Lotus/Types/Enemies/Acolytes/StrikerAcolyteAgent': '',
+        '/Lotus/Types/Enemies/Acolytes/ControlAcolyteAgent': ''}
+cnt = 0
+
 
 def data():
     from urllib.request import urlopen
@@ -35,49 +40,55 @@ def check(raw_data):
     t = json.loads(raw_data)
     t2 = (dict(t['PersistentEnemies'][0]))
     msg = t2['Discovered']
-    print(msg)
     name = (t2['AgentType'])
     if msg:
         print(names[name], ' found in: ', t2['LastDiscoveredLocation'])
         dts[name] += 1
+        nods[name] = t2['LastDiscoveredLocation']
     else:
         dts[name] -= dts[name]
+        nods[name] = ''
 
 
 def check2(raw_data):
     t = json.loads(raw_data)
     t2 = (dict(t['PersistentEnemies'][1]))
     msg = t2['Discovered']
-    print(msg)
     name = (t2['AgentType'])
     if msg:
         print(names[name], ' found in: ', t2['LastDiscoveredLocation'])
         dts[name] += 1
+        nods[name] = t2['LastDiscoveredLocation']
     else:
         dts[name] -= dts[name]
+        nods[name] = ''
 
 
 def check3(raw_data):
     t = json.loads(raw_data)
     t2 = (dict(t['PersistentEnemies'][2]))
     msg = t2['Discovered']
-    print(msg)
     name = (t2['AgentType'])
     if msg:
         print(names[name], ' found in: ', t2['LastDiscoveredLocation'])
         dts[name] += 1
+        nods[name] = t2['LastDiscoveredLocation']
     else:
         dts[name] -= dts[name]
+        nods[name] = ''
 
 
 def checker():
     answ = ''
     if dts['/Lotus/Types/Enemies/Acolytes/HeavyAcolyteAgent'] == 1:
-        answ += ' Malice Found '
+        answ += 'Malice Found' + ' ' + str(cnt) + ' ' + nods[
+            '/Lotus/Types/Enemies/Acolytes/HeavyAcolyteAgent'] + '\n'
     if dts['/Lotus/Types/Enemies/Acolytes/StrikerAcolyteAgent'] == 1:
-        answ += ' Angst Found '
+        answ += 'Angst Found' + ' ' + str(cnt) + ' ' + nods[
+            '/Lotus/Types/Enemies/Acolytes/StrikerAcolyteAgent'] + '\n'
     if dts['/Lotus/Types/Enemies/Acolytes/ControlAcolyteAgent'] == 1:
-        answ += ' Torment Found'
+        answ += 'Torment Found' + ' ' + str(cnt) + ' ' + nods[
+            '/Lotus/Types/Enemies/Acolytes/ControlAcolyteAgent'] + '\n'
     return answ
 
 
@@ -106,7 +117,14 @@ def main():
         pass
 
 
+def db():
+    answ = checker()
+    with open('data.txt', 'a') as f:
+        f.write(answ)
+
+
 def start():
+    global cnt
     print('Checking')
     data()
     time.sleep(3)
@@ -117,9 +135,10 @@ def start():
     time.sleep(1)
     check3(raw_data)
     time.sleep(1)
-    print(dts)
+    db()
     main()  # Delete this to remove twitter notification
     time.sleep(60)
+    cnt += 1
     start()
 
 
